37fa672286e400869ccd122166ed9cdc532c1d02
Kim Pham <ms.kimpham@gmail.com>
2015-04-13 15:12:57 -0700

    Data persist in Firebase

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,17 +8,28 @@
  * Controller of the earnestkpApp
  */
 angular.module('earnestkpApp')
-  .controller('MainCtrl', function ($scope) {
-  
-  $scope.users = {
-    1: {name: 'Louis Beryl', roles: [3]},
-    2: {name: 'Brian Romanko', roles: [2]},
-    3: {name: 'Scott Turnquest', roles: [2,3]},
-    4: {name: 'Kimberly Maher', roles: [1,3]},
-    5: {name: 'Kim Pham', roles: [4]}
+  .controller('MainCtrl', function ($scope, Users, $window) {
+
+  $scope.seedData = function() {
+    var ref = new $window.Firebase('https://earnestkp.firebaseio.com/users');
+    ref.push({
+      name : 'Louis Beryl',
+      roles : [1,2]
+    });
+    ref.push({
+      name : 'Brian Romanko',
+      roles : [2]
+    });
+    ref.push({
+      name : 'Scott Turnquest',
+      roles : [2,3]
+    });
+    console.log('clicked');
   };
 
-  var usersCount = 5;
+  $scope.users = Users.fbObject;
+
+  var usersCount = 3;
 
   $scope.roles = [
     {value: 1, text: 'User'},
@@ -26,14 +37,11 @@ angular.module('earnestkpApp')
     {value: 3, text: 'Admin'},
     {value: 4, text: 'Guest'}
   ]; 
-  
-  $scope.checkName = function(data, id) {
-    console.log(data + id);
-  };
 
   $scope.saveUser = function(data, id) {
-    console.log(data);
-    $scope.users[id] = data;
+    console.log(data, id);
+    Users.fbArray[id] = data;
+    Users.fbArray.$save();
   };
 
   $scope.showRole = function(id) {
@@ -43,7 +51,7 @@ angular.module('earnestkpApp')
         selected.push(s.text);
       }
     });
-    return selected.length ? selected : 'Not set';
+    return selected;
   }; 
 
   $scope.removeUser = function(id) {
@@ -58,53 +66,5 @@ angular.module('earnestkpApp')
     };
     $scope.users[usersCount] = $scope.inserted;
   };
-
-
-  //   $scope.users = Users.fbObject;
-    
-  //   $scope.roles = [
-  //     {id: 1, text: 'guest'},
-  //     {id: 2, text: 'user'},
-  //     {id: 3, text: 'superuser'},
-  //     {id: 4, text: 'admin'}
-  //   ];
-  //   $scope.userRole = {};
-
-  //   $scope.showStatus = function() {
-  //   var selected = [];
-  //   angular.forEach($scope.statuses, function(s) { 
-  //     if ($scope.user.status.indexOf(s.value) >= 0) {
-  //       selected.push(s.text);
-  //     }
-  //   });
-  //   return selected.length ? selected.join(', ') : 'Not set';
-  // }; 
-
-  //   $scope.addRole = function(role) {
-  //     if (!$scope.userRole[role]) {
-  //       $scope.userRole[role] = true;
-  //     } else {
-  //       delete $scope.userRole[role];
-  //     }
-  //   };
-
-  //   $scope.editUser = function(data, id) {
-  //     console.log(data);
-  //     $scope.users[id] = data;
-  //     $scope.users.$save();
-  //   };
-
-  //   $scope.addUser = function() {
-  //     Users.fbArray.$add({
-  //       firstName: $scope.firstName,
-  //       roles: $scope.userRole
-  //     });
-  //     // clear form input
-  //     $scope.firstName = '';
-  //   };
-
-  //   $scope.removeUser = function($index) {
-  //     Users.fbArray.$remove($index);
-  //   };
 
   });
